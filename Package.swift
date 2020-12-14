893bac1d196a211ecde4f7ceb055f3103a3ab1c2
Andre Weinkötz <andre.weinkoetz@tum.de>
2020-12-14 10:50:26 +0100

    updated dependency

--- Package.swift
+++ Package.swift
@@ -16,9 +16,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/tum-aweink/OpenAPIKit.git", .exact("2.0.0")),
-        .package(url: "https://github.com/kylef/PathKit.git", .exact("0.9.2")),
-        .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0")
+        .package(url: "https://github.com/mattpolzin/OpenAPIKit.git", from: "2.0.0"),
+        .package(url: "https://github.com/kylef/PathKit.git", .exact("0.9.2"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -27,8 +26,7 @@ let package = Package(
             name: "PallidorGenerator",
             dependencies: [
                 .product(name: "OpenAPIKit", package: "OpenAPIKit"),
-                .product(name: "PathKit", package: "PathKit"),
-                .product(name: "Yams", package: "Yams")
+                .product(name: "PathKit", package: "PathKit")
         ],
             resources: [
                 .process("NetworkingTemplates/HTTPAuthorizationModel.md"),
